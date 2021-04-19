d8861274bbd0dacd21e97bea94a2c6f4aa020a8e
Milos Petrusic <petrusicmilos94@gmail.com>
2021-04-19 16:02:15 +0200

    Initial Commit

---
+++ KiwiFlights/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KiwiFlights
+//
+//  Created by Milos Petrusic on 19.4.21..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
